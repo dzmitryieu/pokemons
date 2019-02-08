17c3794616e6169e91abd47e2b7742f42e9e8f8a
Viktar Dzmitryieu <Viktar_Dzmitryieu@epam.com>
2019-02-08 17:30:29 +0300

    process

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -6,14 +6,14 @@ const Add = () => (
   <div className={styles.wrapper}>
     <form>
       <h2>Name:</h2>
-      <input type="text" name="name" defaultValue="Enter name" />
+      <input type="text" name="name" placeholder="Enter name" />
       <h2>Base Expirience:</h2>
-      <input type="text" name="base_expirience" defaultValue="Enter number" />
+      <input type="text" name="base_expirience" placeholder="Enter number" />
       <h2>Height:</h2>
-      <input type="text" name="height" defaultValue="Enter number" />
+      <input type="text" name="height" placeholder="Enter number" />
       <h2>Image URL:</h2>
-      <input type="text" name="height" defaultValue="Enter URL" />
-      <input className={styles.button} type="submit" value="Add Pokemon" />
+      <input type="text" name="height" placeholder="Enter URL" />
+      <button className={styles.button} type="submit">Add Pokemon</button>
     </form> 
   </div>
 )
--- src/components/Change/Change.jsx
+++ src/components/Change/Change.jsx
@@ -6,35 +6,63 @@ class Change extends React.Component {
 
   state = {
     list: [],
+    name: '',
+    experience: '',
+    height: '',
   }
 
   componentDidMount () {
     fetch('https://pokeapi.co/api/v2/pokemon/')
       .then(response => response.json())
-      .then(data => data.results.map(el => el.name))
       .then(data => this.setState({
-        list: data
+        list: data.results
       }))      
   }
 
-  render () {
+  onClickHandler = (e) => {
+    console.log("Click!!!");
     const { list } = this.state;
+    const name = e.target.dataset.name;
+    console.log(name);
+    const pokemon = list.find(el => el.name === name);
+    fetch(pokemon.url)
+      .then(response => response.json())
+      .then(data => this.setState({
+        name: name,
+        experience: data.base_experience,
+        height: data.height,
+      }))   
+  }
+
+  render () {
+    const { 
+      list,
+      name,
+      experience,
+      height
+     } = this.state;
     return (
       <div className={styles.wrapper}>
         <form>
           <h2>Choose Pokemon:</h2>
           <select className={styles.list} type="text" name="name">
             {list && list.map(el => (
-              <option key={el}>{el}</option>
+              <option 
+                key={el.name} 
+                onClick={this.onClickHandler}
+                data-name={el.name}
+              >
+                {el.name}
+              </option>
             ))}
           </select>
           <h2>Change Name:</h2>
-          <input type="text" name="base_expirience" defaultValue="Enter number" />
+          <input type="text" name="base_expirience" placeholder="Enter name" value={name} onChange={this.onChangeHandler} />
           <h2>Change Base Expirience:</h2>
-          <input type="text" name="base_expirience" defaultValue="Enter number" />
+          <input type="text" name="base_expirience" placeholder="Enter number" value={experience} onChange={this.onChangeHandler} />
           <h2>Change Height:</h2>
-          <input type="text" name="height" defaultValue="Enter number" />
-          <input className={styles.button} type="submit" value="Change Pokemon" />
+          <input type="text" name="height" placeholder="Enter number" value={height} onChange={this.onChangeHandler} />
+          <button className={styles.button} type="submit">Change Pokemon</button>
         </form> 
       </div>
     )
